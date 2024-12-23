50390875efc19c4cb25e26b11e509c13eb7ac6a5
Ivan Aranda <zendo108@yahoo.com>
2024-12-22 21:34:36 -0500

    Update pcl-to-pdf.bat
    added for loop to go through all the pcl files

--- pcl-to-pdf.bat
+++ pcl-to-pdf.bat
@@ -3,5 +3,14 @@ REM The follwowing script converts all pcl files to image base pdfs
 for %%I in (*.pcl) do gpcl6win64.exe -dNOPAUSE -sDEVICE=pdfwrite -sOutputFile="%%~nI.pdf" "%%I"
 
 :: Improved suggested script that tries to generate a searchable text pdf format rather than image text
-gpcl6win64.exe -dNOPAUSE -sDEVICE=pdfwrite -sOutputFile="output.pdf" \
--dEmbedAllFonts=true -dSubsetFonts=true -dUseCIEColor input.pcl
+@echo off
+for %%I in (*.pcl) do (
+    gpcl6win64.exe -dNOPAUSE -sDEVICE=pdfwrite ^
+    -sOutputFile="%%~nI.pdf" ^
+    -dEmbedAllFonts=true ^
+    -dSubsetFonts=true ^
+    -dUseCIEColor ^
+    -dCompressFonts=true ^
+    -dAutoRotatePages=/PageByPage ^
+    "%%I"
+)
